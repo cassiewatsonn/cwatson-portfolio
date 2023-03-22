c0c48ef017b169bcac7c553ae234efc12c790d68
cassiewatsonn <121472771+cassiewatsonn@users.noreply.github.com>
2023-03-22 15:44:37 -0400

    added bootstrap

---
+++ src/NavTabs.js
@@ -0,0 +1,58 @@
+import React from 'react';
+
+// destructuring the props object into thw properties current page and handle page change. 
+// allows us to access values directly wihout referencing props everytime 
+
+function NavTabs({ currentPage, handlePageChange }) {
+  return (
+    <ul className="nav nav-tabs">
+      <li className="nav-item">
+        <a
+          href="#home"
+          onClick={() => handlePageChange('Home')}
+
+
+          className={currentPage === 'Home' ? 'nav-link active' : 'nav-link'}
+        >
+          Home
+        </a>
+      </li>
+      <li className="nav-item">
+        <a
+          href="#about"
+          onClick={() => handlePageChange('About')}
+
+
+          className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}
+        >
+          About
+        </a>
+      </li>
+      <li className="nav-item">
+        <a
+          href="#blog"
+          onClick={() => handlePageChange('Blog')}
+
+
+          className={currentPage === 'Blog' ? 'nav-link active' : 'nav-link'}
+        >
+          Blog
+        </a>
+      </li>
+      <li className="nav-item">
+        <a
+          href="#contact"
+
+          // changing style for that tab changing class to active or non active 
+
+          onClick={() => handlePageChange('Contact')}
+          className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
+        >
+          Contact
+        </a>
+      </li>
+    </ul>
+  );
+}
+
+export default NavTabs;
