54b20dfba32440723167e17ded70266016149ac7
cassiewatsonn <121472771+cassiewatsonn@users.noreply.github.com>
2023-03-22 15:54:57 -0400

    worked on PortfolioContainer

--- src/components/PortfolioContainer.js
+++ src/components/PortfolioContainer.js
@@ -1,6 +1,36 @@
-import React from "react";
-import PortfolioContainer from "./components/PortfolioContainer";
+import React, { useState } from 'react';
+import Nav from './Nav';
+import AboutMe from './pages/AboutMe';
+import Portfolio from './pages/Portfolio';
+import Resume from './pages/Resume';
+import ContactMe from './pages/ContactMe';
 
-const App = () => <PortfolioContainer />;
+export default function PortfolioContainer() {
+  const [currentPage, setCurrentPage] = useState('Home');
 
-export default App;
+  // conditional operator to check which page is the current page in order to render it properly
+  const renderPage = () => {
+    if (currentPage === 'AboutMe') {
+      return <AboutMe />;
+    }
+    if (currentPage === 'Portfolio') {
+      return < Portfolio/>;
+    }
+    if (currentPage === 'Resume') {
+      return <Resume />;
+    }
+    return <ContactMe />;
+  };
+
+  const handlePageChange = (page) => setCurrentPage(page);
+
+  return (
+    <div>
+
+      <Nav currentPage={currentPage} handlePageChange={handlePageChange} />
+
+      {renderPage()}
+    </div>
+  );
+}
+
