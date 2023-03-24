adbb07f4a9624445e823f274c972d590509b0553
cassiewatsonn <121472771+cassiewatsonn@users.noreply.github.com>
2023-03-24 14:18:10 -0400

    styled contact me page. problem with macking card background blue

--- src/components/pages/AboutMe.js
+++ src/components/pages/AboutMe.js
@@ -11,11 +11,11 @@ export default function AboutMe() {
           
           <h5 className="bio">Web Developer with a Passion for Enhancing Lives - Former Recreation Therapist Turned Tech Enthusiast</h5>
           
-          <p className="bio-description">As a former recreation therapist, I have spent years dedicated to improving the lives of others through creative problem-solving and innovative approaches to healthcare. I have always been fascinated by the intersection of technology and healthcare, and I decided to take the leap and pursue a career in web development. 
+          <p className="bio-description">As a former recreation therapist, I have spent years dedicated to improving the lives of others through creative problem-solving and innovative approaches to healthcare. I have always been fascinated by the intersection of technology and healthcare, and I decided to take the leap and pursue a career in web development.</p>
           
-          Currently, I am enrolled in the University of Toronto coding bootcamp where I am developing my skills in HTML, CSS, JavaScript, and much more. I am excited to continue learning and growing as a developer and to apply my background in healthcare to create meaningful, user-focused applications that can make a difference in people's lives. 
+          <p className="bio-description">Currently, I am enrolled in the University of Toronto coding bootcamp where I am developing my skills in HTML, CSS, JavaScript, and much more. I am excited to continue learning and growing as a developer and to apply my background in healthcare to create meaningful, user-focused applications that can make a difference in people's lives.</p> 
           
-          In my free time, you can find me hiking with my dog or trying out new recipes in the kitchen. I am always seeking out new challenges and adventures, and I am excited to see where this new career path takes me.</p>
+          <p className="bio-description">In my free time, you can find me hiking with my dog or trying out new recipes in the kitchen. I am always seeking out new challenges and adventures, and I am excited to see where this new career path takes me.</p>
       </div>
     </div>
     )
