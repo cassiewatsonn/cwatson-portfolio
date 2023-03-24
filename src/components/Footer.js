ebff59e25748dd7bb08c1c646ed3568e4078a92b
cassiewatsonn <121472771+cassiewatsonn@users.noreply.github.com>
2023-03-24 13:32:22 -0400

    styled footer. still need to figure out how to center logos and have space between them

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -19,17 +19,26 @@ export default function Footer() {
     return(
         <footer className="footer">
             <div>
-                <div>
+                <div className="footer-title">
                     <h3>Connect with me!</h3>
                 </div>
                 
-                <div>
-                    <a href="https://github.com/cassiewatsonn" target="_blank"><img width="50px" height="50px" src={require('../mediaimages/github-mark-white.png')} alt="Github"/></a>
-                    <a href="https://www.linkedin.com/in/cassandrawatsonn/" target="_blank"><img width="50px" height="50px" src={require('../mediaimages/linkedin-3-xxl.png')} alt="LinkedIn"/></a>
-                    <a href="https://twitter.com/cassiewatsonnnn" target="_blank"><img width="50px" height="50px" src={require('../mediaimages/Twitter social icons - rounded square - white.png')} alt="Stack-Overflow"/></a>
-                    <a href="https://www.instagram.com/cassiewatsonn/" target="_blank"><img width="50px" height="50px" src={require('../mediaimages/Instagram_Glyph_White.png')} alt="Instagram"/></a>
+                <div className="footer-logos">
+                    {/* <div> */}
+                        <a href="https://github.com/cassiewatsonn" target="_blank"><img width="50px" height="50px" src={require('../mediaimages/github-mark-white.png')} alt="Github"/></a>
+                    {/* </div>
+                    <div> */}
+                        <a href="https://www.linkedin.com/in/cassandrawatsonn/" target="_blank"><img width="50px" height="50px" src={require('../mediaimages/linkedin-3-xxl.png')} alt="LinkedIn"/></a>
+                    {/* </div>
+                    <div> */}
+                        <a href="https://twitter.com/cassiewatsonnnn" target="_blank"><img width="50px" height="50px" src={require('../mediaimages/Twitter social icons - rounded square - white.png')} alt="Twitter"/></a>
+                    {/* </div>
+                    <div> */}
+                        <a href="https://www.instagram.com/cassiewatsonn/" target="_blank"><img width="50px" height="50px" src={require('../mediaimages/Instagram_Glyph_White.png')} alt="Instagram"/></a>
+                    {/* </div> */}
+                
                 </div>
-                <div>
+                <div className="copyright">
                     {`Copyright @ ${year} Cassandra Watson`}
                 </div>
             </div>
